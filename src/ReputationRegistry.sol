e2307bcd70fcf3ee8d6b56dd1b17ae903f74b021
hectiik <mlghectiik@gmail.com>
2025-08-19 16:20:03 -0400

    Feat: Add Temporary Title Natspec Comment

--- src/ReputationRegistry.sol
+++ src/ReputationRegistry.sol
@@ -4,4 +4,9 @@ pragma solidity ^0.8.19;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title ReputationRegistry
+ * @dev Core contract for managing decentralized reputation scores
+ * @notice This contract stores and calculates reputation scores with anti-gaming mechanisms
+ */
 contract ReputationRegistry {}
