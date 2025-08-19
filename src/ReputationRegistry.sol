f0e70c589349231d55c2d7dbaecd8b6f806fb424
hectiik <mlghectiik@gmail.com>
2025-08-19 16:19:40 -0400

    Feat: Add Necessary Imports

--- src/ReputationRegistry.sol
+++ src/ReputationRegistry.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 contract ReputationRegistry {}
