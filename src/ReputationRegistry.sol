5735db53de8e6e84d5e6ed2ec0e925dee1d487ab
hectiik <mlghectiik@gmail.com>
2025-08-20 10:52:05 -0400

    Feat: Add ReputationParametersUpdated Event

--- src/ReputationRegistry.sol
+++ src/ReputationRegistry.sol
@@ -53,5 +53,8 @@ contract ReputationRegistry is Ownable, ReentrancyGuard {
     event ReputationDecayed(address indexed user, uint256 oldScore, uint256 newScore);
     event AuthorizedRaterAdded(address indexed rater);
     event AuthorizedRaterRemoved(address indexed rater);
-
+    event ReputationParametersUpdated(
+        uint256 minRaterReputation,
+        uint256 maxWeightMultiplier
+    );
 }
