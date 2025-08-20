4e5eec9fe257389932e4b156dfd1af611ab79b7d
hectiik <mlghectiik@gmail.com>
2025-08-20 10:51:43 -0400

    Feat: Add AuthorizedRaterRemoved Event

--- src/ReputationRegistry.sol
+++ src/ReputationRegistry.sol
@@ -52,5 +52,6 @@ contract ReputationRegistry is Ownable, ReentrancyGuard {
     );
     event ReputationDecayed(address indexed user, uint256 oldScore, uint256 newScore);
     event AuthorizedRaterAdded(address indexed rater);
+    event AuthorizedRaterRemoved(address indexed rater);
 
 }
