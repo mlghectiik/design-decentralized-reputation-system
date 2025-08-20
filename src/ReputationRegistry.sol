4d5eea585e643b6d46b89f6b8718cdee2d4cea68
hectiik <mlghectiik@gmail.com>
2025-08-20 10:57:07 -0400

    Feat: Add registerUser Admin Func Natspec Comments

--- src/ReputationRegistry.sol
+++ src/ReputationRegistry.sol
@@ -68,6 +68,10 @@ contract ReputationRegistry is Ownable, ReentrancyGuard {
 
     constructor(address _owner) Ownable(_owner) {}
 
+    /**
+     * @dev Register a new user in the reputation system
+     * @param user Address of the user to register
+     */
     function registerUser(address user) external onlyOwner {
         if (_reputations[user].isRegistered) {
             revert UserAlreadyRegistered(user);
