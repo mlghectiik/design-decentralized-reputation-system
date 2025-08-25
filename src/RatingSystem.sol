e1a13f6b0950b534f343886f84a64b5c16f70d68
hectiik <mlghectiik@gmail.com>
2025-08-25 17:37:54 -0400

    Feat: Add Contract Inheritance

--- src/RatingSystem.sol
+++ src/RatingSystem.sol
@@ -23,4 +23,4 @@ interface IReputationRegistry {
  * @dev Handles rating submissions between users with anti-gaming mechanisms
  * @notice This contract manages the rating process and enforces business rules
  */
-contract RatingSystem {}
+contract RatingSystem is Ownable, ReentrancyGuard, Pausable {}
