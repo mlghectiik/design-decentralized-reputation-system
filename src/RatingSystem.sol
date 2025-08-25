1273a6f3180186cee279b7544323ea29688eba0c
hectiik <mlghectiik@gmail.com>
2025-08-25 17:36:38 -0400

    Feat: Add Necessary Imports

--- src/RatingSystem.sol
+++ src/RatingSystem.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+
 contract RatingSystem {}
