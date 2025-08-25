f258ca2dd534e64b8ed569da0e0c4b631c3bbda5
hectiik <mlghectiik@gmail.com>
2025-08-25 17:36:57 -0400

    Feat: Add IReputationRegistry Interface

--- src/RatingSystem.sol
+++ src/RatingSystem.sol
@@ -5,4 +5,17 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 
+interface IReputationRegistry {
+    function getReputation(address user) external view returns (uint256);
+    function updateReputation(address user, uint256 rating, address rater) external;
+    function getReputationData(address user) external view returns (
+        uint256 score,
+        uint256 totalRatings,
+        uint256 totalScore,
+        uint256 lastUpdateTime,
+        uint256 lastDecayTime,
+        bool isRegistered
+    );
+}
+
 contract RatingSystem {}
